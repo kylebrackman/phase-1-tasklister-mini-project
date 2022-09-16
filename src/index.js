108c3cfe8ec4732238988666bea027a683efd150
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-09-16 13:43:13 -0600

    Initialize brackman task website

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => { 
+    e.preventDefault()
+    buildTask(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function buildTask(task) {
+  let p = document.createElement('p')
+  p.textContent = task
+  document.querySelector('#list').appendChild(p) 
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteButton)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  console.log(p)
+}
+
+function deleteButton(e) {
+  e.target.parentNode.remove()
+}
